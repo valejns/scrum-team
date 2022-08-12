dc2245080cceb75bbfca08c4a7dcf22eed7ad6b4
Jesus Bilchi <106125867+jesubilchi@users.noreply.github.com>
2022-08-12 20:19:50 -0300

    Update ejercicio4.py

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,5 +1,5 @@
-perro = ['14', 'Fido', '12/12/2012', 'Macho', '23546987']
-n = len(perro)
-for i in range(len(perro)):
+perro2 = ['14', 'Fido', '12/12/2012', 'Macho', '23546987']
+n = len(perro2)
+for i in range(len(perro2)):
     n = n-1
-    print(perro[n],end=' ')
+    print(perro2[n],end=' ')
